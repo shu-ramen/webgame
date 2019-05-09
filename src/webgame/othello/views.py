461d8d7a4e4729b202f9c0136ced8cdf03c69399
shu-ramen <shu.h7.research@gmail.com>
2019-05-09 19:49:13 +0900

    オセロのトップページを編集

--- src/webgame/othello/views.py
+++ src/webgame/othello/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('othello/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
