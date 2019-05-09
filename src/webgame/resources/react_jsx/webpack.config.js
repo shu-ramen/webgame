d0d84c5566fc169638cb063e20f080d18d55749d
shu-ramen <shu.h7.research@gmail.com>
2019-05-09 19:50:18 +0900

    webpack.config.jsを修正

--- src/webgame/resources/react_jsx/webpack.config.js
+++ src/webgame/resources/react_jsx/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     entry: {
         share_header:  path.resolve(__dirname, "src/share/header.jsx"),
         home_index:    path.resolve(__dirname, "src/home/index.jsx"),
+        othello_index: path.resolve(__dirname, "src/othello/index.jsx"),
     },
     output: {
         path: path.resolve(__dirname, "../static/js/") ,
