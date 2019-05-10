2ce423d98b75b529a37bca0a1652e77ab9a123e8
shu-ramen <shu.h7.research@gmail.com>
2019-05-10 09:29:43 +0900

    ユーザー登録・ログイン機能を実装

--- src/webgame/othello/views.py
+++ src/webgame/othello/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def index(request):
     template = loader.get_template('othello/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
+@login_required
 def vscpu(request, level):
     print(level)
     template = loader.get_template('othello/vscpu.html')
--- src/webgame/webgame/settings.py
+++ src/webgame/webgame/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'resources/static'),
 )
+
+LOGIN_REDIRECT_URL = '/'
